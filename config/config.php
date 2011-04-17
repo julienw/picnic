40a796d6ce4ccd5c4b14584ae121fcf9f68876b4
Julien Wajsberg <felash@gmail.com>
2011-04-17 23:16:03 +0200

    minor changes and added twig

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+define('TEMPLATES_PATH', '/home/julien/perso/picnic/templates');
+define('TEMPLATES_CACHE', '/home/julien/perso/picnic/cache');
+
+
--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
-
+require_once 'config/config.php';
 require_once 'includes/toro.php';
+
+require_once 'includes/Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem(TEMPLATES_PATH);
+$twig = new Twig_Environment($loader, array(
+    'cache' => TEMPLATES_CACHE,
+    'strict_variables' => true
+));
 
 
 class MainHandler extends ToroHandler {
@@ -9,8 +18,15 @@ class MainHandler extends ToroHandler {
     }
 }
 
+class MainHandler2 extends ToroHandler {
+    public function get() { 
+        echo 'Hello, world too !';
+    }
+}
+
 $site = new ToroApplication(array(
-    array('/', 'MainHandler')
+    array('/', 'MainHandler'),
+    array('/too', 'MainHandler2')
 ));
 
 $site->serve();
